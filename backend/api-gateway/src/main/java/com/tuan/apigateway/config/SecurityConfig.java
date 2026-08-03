eab1e56a0f9cfabdb59bf7d2d85496686784732c
Le Anh Tuan <leanhtuan.18.03.03@gmail.com>
2026-08-03 12:15:12 +0700

    config docker

--- backend/api-gateway/src/main/java/com/tuan/apigateway/config/SecurityConfig.java
+++ backend/api-gateway/src/main/java/com/tuan/apigateway/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
