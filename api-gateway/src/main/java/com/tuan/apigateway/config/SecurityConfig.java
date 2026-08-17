c2f9f76046803e03774c44fc7e456d9410e10ae4
Le Anh Tuan <leanhtuan.18.03.03@gmail.com>
2026-08-17 13:45:03 +0700

    update api-gateway config routes

--- api-gateway/src/main/java/com/tuan/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/tuan/apigateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,7 @@ import org.springframework.web.server.ServerWebExchange;
 import java.util.List;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
